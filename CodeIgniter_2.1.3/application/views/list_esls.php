5996114fc40da922e7606640ea90431f6010c332
alexDev <alexdev@vBox.(none)>
2013-03-11 23:57:08 -0600

    fixed link to generate new esl

--- CodeIgniter_2.1.3/application/views/list_esls.php
+++ CodeIgniter_2.1.3/application/views/list_esls.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
  <body>
-    <a href=<?php echo site_url(); ?>'home/makeEsl'>Generate a new ESL<a><br><br>
+    <a href=<?php echo base_url(); ?>'home/makeEsl'>Generate a new ESL<a><br><br>
 
    ESLs:<br>
    <?php foreach ($esls as $esl):
