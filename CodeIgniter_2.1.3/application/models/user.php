13dd56eb61d9299f530ab1deb207855bb1c54276
alexDev <alexdev@vBox.(none)>
2013-03-26 09:39:36 -0600

    debug

--- CodeIgniter_2.1.3/application/models/user.php
+++ CodeIgniter_2.1.3/application/models/user.php
@@ -132,7 +132,7 @@ Class User extends CI_Model
 
         function saveFoursquareToken($username = '', $token = '')
         {
-            var_dump($username);
+            var_dump($token);
             $db = new PDO('sqlite:./application/db/deliveryDrivers');
             $db->query("UPDATE Users SET fsAccessToken='".$token."' WHERE username='".$username."';");
         }
