c32638c1e2230e4e31077dbcfd8cd80e746ae13b
alexDev <alexdev@vBox.(none)>
2013-03-26 09:38:05 -0600

    redirect to foursquare instead of curl

--- CodeIgniter_2.1.3/application/controllers/driver.php
+++ CodeIgniter_2.1.3/application/controllers/driver.php
@@ -29,14 +29,16 @@ class Driver extends CI_Controller {
 
         function foursquareAuth()
         {
-            $fields_str = "client_id=Y4XZ44AUGUG031Q0A0Y0LVYIJA2IFU4XMAYZ4QGTJIOSL2I3&return_type=code&redirect_uri=https://students.cs.byu.edu/~kdevenis/CS462-driver/drivers/CodeIgniter_2.1.3/index.php/driver/code";
+            redirect('https://foursquare.com/oauth2/authenticate?client_id=Y4XZ44AUGUG031Q0A0Y0LVYIJA2IFU4XMAYZ4QGTJIOSL2I3&response_type=code&redirect_uri=https://students.cs.byu.edu/~kdevenis/CS462-driver/drivers/CodeIgniter_2.1.3/index.php/driver/code');
+
+            /*$fields_str = "client_id=Y4XZ44AUGUG031Q0A0Y0LVYIJA2IFU4XMAYZ4QGTJIOSL2I3&return_type=code&redirect_uri=https://students.cs.byu.edu/~kdevenis/CS462-driver/drivers/CodeIgniter_2.1.3/index.php/driver/code";
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL, "https://foursquare.com/oauth2/authenticate");
                 curl_setopt($ch, CURLOPT_POST, 3);
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_str);
                 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
                 curl_exec($ch);
-                curl_close($ch);
+                curl_close($ch);*/
         }
 
         function code()
--- CodeIgniter_2.1.3/application/models/user.php
+++ CodeIgniter_2.1.3/application/models/user.php
@@ -132,7 +132,7 @@ Class User extends CI_Model
 
         function saveFoursquareToken($username = '', $token = '')
         {
-            var_dump("here");
+            var_dump($username);
             $db = new PDO('sqlite:./application/db/deliveryDrivers');
             $db->query("UPDATE Users SET fsAccessToken='".$token."' WHERE username='".$username."';");
         }
