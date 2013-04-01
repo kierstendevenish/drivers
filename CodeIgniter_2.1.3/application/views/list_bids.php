7362b1e5f6991b2ce1418b309d025e103070b0c9
alexDev <alexdev@vBox.(none)>
2013-04-01 15:21:20 -0600

    debug

--- CodeIgniter_2.1.3/application/views/list_bids.php
+++ CodeIgniter_2.1.3/application/views/list_bids.php
@@ -14,9 +14,10 @@
            <th>Delivery Time</th>
        </tr>
    <?php if (count($bids) > 0):
-        foreach ($bids as $bid):
+        echo $bids;
+        /*foreach ($bids as $bid):
             echo "<tr><td>" . $bid['fs_esl'] . "</td><td>" . $bid['delivery_id'] . "</td><td>" . $bid['pickupTime'] . "</td><td>".$bid['deliveryAddr']."</td><td>".$bid['deliveryTime']."</td></tr>";
-        endforeach;
+        endforeach;*/
    endif; ?>
    </table>
  </body>
